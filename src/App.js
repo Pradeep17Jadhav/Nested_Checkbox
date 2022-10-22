5e5a87d99ba5073f33a03f87362dca4c1b69f5db
Pradeep17Jadhav <7hqcKS5p>
2022-10-22 17:40:32 +0530

    remove extra code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Checkbox component
+    <div className="app">
+      <header className="header">
+        Nested Checkbox
       </header>
     </div>
   );
